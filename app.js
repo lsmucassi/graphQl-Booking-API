75c06e17742c357e53ed5ec128c5de9b724eed05
lsmucassi <lindasmucassi@gmail.com>
2020-04-20 16:33:28 +0200

    schema setup- weird theeengs

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const graphQlHttp = require('express-graphql')
+const { buildSchema } = require('graphql')
+
 
 const app = express()
+const PORT = 3000
+
+app.use('/graphql', graphQlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            event: [String!]
+        }
+        type RootMutation {
+             createEvents(name: String): String
+        }
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {},
+}))
+
 
 app.use(bodyParser.json())
-app.listen(3000, () => console.log('APP: Running Server on port 3000'))
+app.listen(PORT, () => console.log(`APP: Server running on port ${PORT}`))  
